a07aa8259d1649626336e444c208008c0b59ef43
niklasgunzenheimer-eng <niklasgunzenheimer@gmail.com>
2026-03-29 15:23:25 +0200

    Update page.tsx

--- src/app/kunden/page.tsx
+++ src/app/kunden/page.tsx
@@ -45,9 +45,12 @@ export default function KundenPage() {
         <div className="flex justify-between items-center">
           <h1 className="text-3xl font-bold">Kunden</h1>
 
-          <button className="bg-blue-600 text-white px-4 py-2 rounded-lg text-sm">
-            Kunde hinzufügen
-          </button>
+          <Link
+  href="/kunden/neu"
+  className="bg-blue-600 text-white px-4 py-2 rounded-lg text-sm"
+>
+  Kunde hinzufügen
+</Link>
         </div>
 
         {/* LISTE */}
